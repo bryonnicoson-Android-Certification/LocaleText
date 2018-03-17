20117f4806933d9e33e008374b88a8989a8679fe
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-16 19:28:20 -0500

    layouts ltr & rtl

--- app/src/main/java/com/example/android/localetext/MainActivity.java
+++ app/src/main/java/com/example/android/localetext/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.localetext;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -65,6 +66,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Show device Locale settings
+     */
+    private void showLocale() {
+        Intent languageIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+        startActivity(languageIntent);
+    }
+
+    /**
      * Creates the options menu and returns true.
      *
      * @param menu       Options menu
@@ -86,11 +95,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle options menu item clicks here.
-        int id = item.getItemId();
-        if (id == R.id.action_help) {
-            Intent intent = new Intent(this, HelpActivity.class);
-            startActivity(intent);
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_help:
+                showHelp();
+                return true;
+            case R.id.action_language:
+                showLocale();
+                return true;
+            default:
+                // nada
         }
         return super.onOptionsItemSelected(item);
     }
